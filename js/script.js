e9278316205b7d9d935e8bd7be93622320ad92c1
LucasBanega <lucasabanega@gmail.com>
2021-12-26 12:46:42 -0300

    actualización js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let cliente = prompt("¿Es usted cliente de Oxi Real?")
+let mensaje = console.log("Bienvenido");
+
+let nombreCompleto = nombre +" "+ apellido;  
+
+let alerta = alert("Gracias por su visita");
+
+
+let precioBruto = 1000; 
+let IVA = 0.21;
+
+let ivaProducto = precioBruto*IVA; 
+let precioFinal = precioBruto + ivaProducto; 
+

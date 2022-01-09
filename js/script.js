0e0e8794e2143c7d11f6eb349318172db47a3776
LucasBanega <lucasabanega@gmail.com>
2022-01-09 16:26:00 -0300

    actualización js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,40 @@ if (nombreUsuario == "") {
 else {
     alert("Nombre de usuario ingresado" +" "+ nombreUsuario);
 }
+
+/*/ function saludar () {
+
+   console.log("Hola!");
+}
+
+saludar();
+
+function solicitarNombre() {
+    let nombreIngresado = prompt ("Ingrese su nombre");
+    alert("El nombre ingresado es "+ nombreIngresado);
+}
+
+for (let index = 0; index < 30; index++) {
+  solicitarNombre();
+}
+
+function sumar(numero1,numero2){
+    let resultado = numero1+numero2;
+    console.log(resultado);
+}
+
+sumar (82000,95000);
+sumar (125,245);
+/*/
+
+for (let index = 1; index <= 3; index++) {
+    let ingresarNombre = prompt("Ingrese su Nombre");
+    alert(" Cliente N° " +index+" Nombre: "+ingresarNombre);
+    
+}
+
+let cookies = prompt("¿Acepta Cookies?");
+while(cookies == "no"){;
+    alert(cookies+" "+"Aceptaste cookies");
+    cookies = prompt("¿Acepta Cookies?")    
+}

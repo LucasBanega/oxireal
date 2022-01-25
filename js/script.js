5284449d09e48521301b4adbb11a87a5bb334f32
LucasBanega <lucasabanega@gmail.com>
2022-01-25 12:55:56 -0300

    simulador

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-class Prospecto {
+/* class Prospecto {
 
     constructor(nombre, edad, cliente, tipo) {
         this.nombre = nombre;
@@ -10,13 +10,13 @@ class Prospecto {
     soldador() {
         if (this.tipo == "Atal") {
             console.log("Soldas con MIG");
-        }else{
+        } else {
             console.log("Soldas con TIG");
         }
     }
 }
 
-const prospecto1 =new Prospecto("Lucas", "23", "Sí", "MIG");
+const prospecto1 = new Prospecto("Lucas", "23", "Sí", "MIG");
 
 prospecto1.tipo = prompt("¿Suelda con Atal o con Argon?");
 
@@ -26,22 +26,22 @@ prospecto1.nombre = prompt("Ingrese su nombre");
 prospecto1.edad = parseInt(prompt("Ingrese su edad"));
 prospecto1.cliente = prompt("¿Es cliente de Oxi Real?");
 
-const productos =[{id: 1, producto: "Máquina" },
-                  {id: 2, producto: "Torchas"},
-                  {id: 3, producto: "Consumibles"},
-                  {id: 4, producto: "Insumos"}];
-      productos.push({id: 5, producto: "Alambre"});              
-                  
-for(const producto of productos) {
+const productos = [{ id: 1, producto: "Máquina" },
+{ id: 2, producto: "Torchas" },
+{ id: 3, producto: "Consumibles" },
+{ id: 4, producto: "Insumos" }];
+productos.push({ id: 5, producto: "Alambre" });
+
+for (const producto of productos) {
     console.log(producto.id);
     console.log(producto.producto);
 }
 
 
-localStorage.setItem('usuario', 'Lucas'); 
+localStorage.setItem('usuario', 'Lucas');
 localStorage.setItem('Edad', '23');
 
-sessionStorage.setItem('Cuotas', [3,6,9,12]);
+sessionStorage.setItem('Cuotas', [3, 6, 9, 12]);
 sessionStorage.setItem('esDarkMode', true);
 
 
@@ -49,11 +49,13 @@ let usuario = localStorage.getItem('usuario');
 console.log(usuario);
 
 let cantCuotas = sessionStorage.getItem('Cuotas').split(",");
-let modo = (sessionStorage.getItem('esDarkMode')=='true'); 
+let modo = (sessionStorage.getItem('esDarkMode') == 'true');
 
 
 for (let i = 0; i < sessionStorage.length; i++) {
     let clave = localStorage.key(i);
-    console.log = ("Clave: "+ clave);
-    console.log = ("Valor: "+ sessionStorage.getItem(clave));
+    console.log = ("Clave: " + clave);
+    console.log = ("Valor: " + sessionStorage.getItem(clave));
 }
+
+*/
